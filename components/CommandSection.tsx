f5fd9f674c175632f48758a8788fbf3e1214a833
melto.sm <meltosm8@gmail.com>
2025-09-26 14:57:32 +0530

    initial commit

--- components/CommandSection.tsx
+++ components/CommandSection.tsx
@@ -20,31 +20,6 @@ const CommandSection = () => {
                     <ReplySection />
                 </div>
             </div>
-            <div className="media mb-4">
-                <Image src="/images/user.jpg" alt="Image" className="img-fluid mr-3 mt-1" style={{width: "45px"}} width={30} height={30} />
-                <div className="media-body">
-                    <h6><a className="text-secondary font-weight-bold" href="">John Doe</a> <small><i>01 Jan 2045</i></small></h6>
-                    <p>Diam amet duo labore stet elitr invidunt ea clita ipsum voluptua, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum.</p>
-                    <button className="btn btn-sm btn-outline-secondary">Reply</button>
-                </div>
-            </div>
-            <div className="media mb-4">
-                <Image src="/images/user.jpg" alt="Image" className="img-fluid mr-3 mt-1" style={{width: "45px"}} width={30} height={30} />
-                <div className="media-body">
-                    <h6><a className="text-secondary font-weight-bold" href="">John Doe</a> <small><i>01 Jan 2045</i></small></h6>
-                    <p>Diam amet duo labore stet elitr invidunt ea clita ipsum voluptua, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum.</p>
-                    <button className="btn btn-sm btn-outline-secondary">Reply</button>
-                </div>
-            </div>
-            <div className="media mb-4">
-                <Image src="/images/user.jpg" alt="Image" className="img-fluid mr-3 mt-1" style={{width: "45px"}} width={30} height={30} />
-                <div className="media-body">
-                    <h6><a className="text-secondary font-weight-bold" href="">John Doe</a> <small><i>01 Jan 2045</i></small></h6>
-                    <p>Diam amet duo labore stet elitr invidunt ea clita ipsum voluptua, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum.</p>
-                    <button className="btn btn-sm btn-outline-secondary">Reply</button>
-                    <ReplySection />
-                </div>
-            </div>
         </div>
     </div>
   )
